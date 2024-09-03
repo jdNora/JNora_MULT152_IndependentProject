e943cb832db1ab7ea95b262025649dfafa181a7c
jdNora <jaydenoraa@gmail.com>
2024-09-02 23:24:39 -0700

    Finished Lesson D

--- JNora_Unit1/Assets/Scripts/PlayerController.cs
+++ JNora_Unit1/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 30.0f;
+    // Movement Speeds
+    private float speed = 30.0f;
+    private float turnSpeed = 30.0f;
+    // Input
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        /* Attempt at implementing proper reverse
+        if (verticalInput != 1)
+        {
+            horizontalInput *= -1;
+        }
+        */
+
+        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
