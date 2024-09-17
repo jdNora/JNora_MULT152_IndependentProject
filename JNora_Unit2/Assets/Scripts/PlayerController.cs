ebe66e18c5b2143531e719be5d6aa31f36e04ac0
jdNora <jaydenoraa@gmail.com>
2024-09-16 21:10:25 -0700

    Unit 2 Lesson A
    Completed

---
+++ JNora_Unit2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 15.0f;
+    public float xRange = 20.0f;
+    float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0));
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
